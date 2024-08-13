e063793da5df6ddda16558d171ee412cd76cf0ec
NineAbyss <113831700+NineAbyss@users.noreply.github.com>
2024-08-13 19:34:34 +0800

    Update gen_data.py

--- models/enhancer/OneForAll/data/single_graph/Cora/gen_data.py
+++ models/enhancer/OneForAll/data/single_graph/Cora/gen_data.py
@@ -25,7 +25,7 @@ def get_logic_label(ordered_txt):
 def get_data(dset):
     cur_path = os.path.dirname(__file__)
     path = os.path.join(cur_path, "cora.pt")
-    data = torch.load(path)
+    data = torch.load('../../../datasets/cora.pt')
     text = data.raw_texts
     label_names = data.label_names
     nx_g = pyg.utils.to_networkx(data, to_undirected=True)
